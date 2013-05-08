fe3dfe256a99223f4ceb33000bdbde0f7c9d0542
Akhil Verghese <akhilcherian@gmail.com>
2013-05-08 23:47:03 +0530

    Solution to Spoj Problem http://www.spoj.com/problems/MLK/

--- MilkTrading/main.cpp
+++ MilkTrading/main.cpp
@@ -6,13 +6,89 @@
 //  Copyright (c) 2013 Akhil Verghese. All rights reserved.
 //
 
+#include<stdio.h>
+#include<stdlib.h>
 #include <iostream>
+#include <cstdio>
 
-int main(int argc, const char * argv[])
+namespace IO
+
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    const int SIZE = 1 << 19;
+    
+    char buff[SIZE], *p = buff + SIZE;
+    
+    char read_char()
+    {
+        if( p == buff + SIZE )
+        {
+            fread( buff, 1, SIZE, stdin );
+            p = buff;
+        }
+        return *(p++);
+    }
+    
+    inline int read_int()
+    {
+        char c;
+        bool flag=true;
+        while( !isdigit( c = read_char() ) )if(c=='-') flag=false;
+        int r = c-'0';
+        while( isdigit( c = read_char() ) ) r = 10*r + c - '0';
+        if(flag) return r;
+        else return -r;
+    }
 }
 
+using namespace IO;
+using namespace std;
+
+inline int min(int a,int b)
+{
+    return (a<b)?(a):(b);
+}
+
+int milk[1000003];
+
+int main()
+{
+    int n,i,j;
+    n=read_int();
+    
+    for(i=0;i<n;i++)
+        milk[i]=read_int();
+    
+    long long t,cost=0;
+    
+    for(i=0,j=0;i<n;i++)
+    {
+        while(milk[i]<0)
+            i++;
+        
+        while(j<i)
+        {
+            if(milk[j]<0)
+            {
+                t=min(-milk[j],milk[i]);
+                milk[j]+=t;
+                milk[i]-=t;
+                cost+=t*(i-j);
+                if(milk[i]==0)
+                    break;
+            }
+            j++;
+        }
+        
+        if(i+1<n)
+        {
+            milk[i+1]+=milk[i];
+            cost+=milk[i];
+            milk[i]=0;
+        }
+    }
+    
+    printf("%lld",cost);
+    
+    return 0;
+}
